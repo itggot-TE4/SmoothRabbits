b7ad3fdf7dd03ceabf9fe039a183f61fde077d1e
Jacob Sandström <jojacocobob@gmail.com>
2020-11-24 12:10:17 +0100

    fixes #14

---
+++ vue/src/components/LogQAndA.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-card
+    outlined
+    >
+      <v-card-title>{{ question }}</v-card-title>
+      <v-card-subtitle>{{ answer }}</v-card-subtitle>
+    </v-card>
+  </div>
+  
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "LogQAndA",
+  props: ["question", "answer"],
+
+  data: () => ({
+  })
+});
+</script>
