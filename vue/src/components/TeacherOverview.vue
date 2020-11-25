47fcb0fadfdaea49572da65e357e2c8db2163f10
zander <alexander.kjellberg2@elev.ga.ntig.se>
2020-11-25 17:40:59 +0100

    fixed #53

---
+++ vue/src/components/TeacherOverview.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-card class="pa-2">
+        <h1>Teachers</h1>
+
+        <v-divider class="my-7"></v-divider>
+        <TeacherRow />
+
+        
+        <v-divider class="my-10"></v-divider>
+        <TeacherCreate />
+    </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import TeacherRow from "@/components/TeacherRow.vue";
+import TeacherCreate from "@/components/TeacherCreate.vue";
+
+export default Vue.extend({
+    name: 'TeacherOverview',
+
+    components: {
+        TeacherRow,
+        TeacherCreate
+    },
+})
+</script>
