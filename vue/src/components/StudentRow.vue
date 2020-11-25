2f01b73789f5ef5c739b529d7be0b33198ac5cc2
zander <alexander.kjellberg2@elev.ga.ntig.se>
2020-11-25 17:42:39 +0100

    fixed #52

--- vue/src/components/StudentRow.vue
+++ vue/src/components/StudentRow.vue
@@ -1,57 +1,28 @@
 <template>
     <v-row 
+    class="d-flex justify-center"
     >
-        <v-col
-        class="d-flex justify-center">
-            <v-col
-            cols="auto"
-            class="d-flex justify-center align-center"
-            >
-                <h3>example@example.com</h3>
-            </v-col>
-
-            <v-col
-            cols="auto"
-            class="d-flex justify-center align-center"
-            >
-                <h3>Example Name</h3>
-            </v-col>
-
-            <v-col
-            cols="auto"
-            class="d-flex justify-center align-center"
-            style="max-width: 192px;"
-            >
-                <v-select 
+        <v-data-table
+        style="width:98%"
+        :headers="headers"
+        :items="students"
+        >
+        <template v-slot:item.teacherName="props">
+            <v-select 
+                style="max-width:192px"
+                v-model="props.item.teacherName"
                 name="selectTeacher" 
                 :items ='SelectableTeachers'
                 ></v-select>
-            </v-col>
-
-            <v-col
-            cols="4"
-            class="d-flex justify-center align-center"
-            >
-                <v-text-field 
-                v-model="password"
-                label="Password"
-                size="36"
-                >
-                </v-text-field>   
-            </v-col>
-
-            <v-col
-            cols="auto"
-            class="d-flex justify-center align-center"
-            >
+        </template>
+        <template v-slot:item.password="props">
+            <v-text-field v-model="props.item.password" label="Password"></v-text-field>
+        </template>
+        <template v-slot:item.resetbtn="props">
+            <v-btn v-model="props.item.resetbtn" elevation="1" color="primary" tile>RESET</v-btn>
+        </template>
             
-                <v-btn
-                elevation="1"
-                color="primary"
-                >RESET
-                </v-btn>
-            </v-col>
-        </v-col>
+        </v-data-table>
     </v-row>
 
 </template>
@@ -60,11 +31,26 @@
 import Vue from 'vue'
 export default Vue.extend({
       data: () => ({
-        show1: false,
-        password: '',
         SelectableTeachers: ['Daniel Berg', 'Linus Styren', 'Fredrik Kronhamn', 'Ola Lindgren'],
+        headers: [
+            { text: 'Email', align: 'start', value: 'email' },
+            { text: 'Name', value: 'name' },
+            { text: 'Teacher', value: 'teacherName' },
+            { text: '', value: 'password', sortable: false,},
+            { text: '', value: 'resetbtn', sortable: false },
+        ],
+        students: [
+          { email: 'sinead.marquez@elev.ga.ntig.se', name: 'Sinead Marquez' },
+          { email: 'hareem.huff@elev.ga.ntig.se', name: 'Hareem Huff' },
+          { email: 'sonny.garrison@elev.ga.ntig.se', name: 'Sonny Garrison' },
+          { email: 'phillippa.brown@elev.ga.ntig.se', name: 'Phillippa Brown' },
+          { email: 'ava-rose.snyder@elev.ga.ntig.se', name: 'Ava-Rose Snyder' },
+          { email: 'albi.rose@elev.ga.ntig.se', name: 'Albi Rose' },
+          { email: 'damian.hernandez@elev.ga.ntig.se', name: 'Damian Hernandez' },
+          { email: 'onur.bird@elev.ga.ntig.se', name: 'Onur Bird' },
+
+        ],
     })
-
-
+  
 })
 </script>
