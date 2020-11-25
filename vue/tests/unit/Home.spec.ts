ba569acd24f4ebc2e3bff7bedb80359d0fb2c1cd
Jacob Sandström <jojacocobob@gmail.com>
2020-11-25 13:18:45 +0100

    test

---
+++ vue/tests/unit/Home.spec.ts
@@ -0,0 +1,12 @@
+import { shallowMount } from "@vue/test-utils";
+import Home from "@/views/Home.vue";
+
+describe("Home.vue", () => {
+  it("renders the correct welcome text", () => {
+    const msg = "Welcome to Log-O-Matic";
+
+    const wrapper = shallowMount(Home);
+
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
