c6f1c832f5e8e5fd44fed7ba9de225398d6d6358
Sebastian <sebastian.utbult@elev.ga.ntig.se>
2020-11-26 10:58:02 +0100

    Logbook

--- vue/src/views/Logbook.vue
+++ vue/src/views/Logbook.vue
@@ -9,18 +9,18 @@
             <!-- QUESTION CARD -->
             <h1 class="ma-10">Log Book</h1>
 
-            <!-- <v-card elevation="4" class="ma-15 pa-5">
+            <v-card v-for="item in items" v-bind:key="item" elevation="4" class="ma-15 pa-5">
 
-                <h2
-                class="font-weight-light"
-                >question?</h2>
+                <h2 class="font-weight-light">
+                    {{ item.question }}
+                </h2>
 
                 <v-textarea
                 solo
                 label="Answer"
                 ></v-textarea>
 
-            </v-card> -->
+            </v-card>
 
             <!-- SAVE BUTTON -->
             <v-card elevation="0" class="ma-10 d-flex align-end flex-column">
@@ -38,10 +38,15 @@
 import Vue from "vue";
 
 export default Vue.extend({
-  name: "SignInPage",
+    name: "Logbook",
+    
+    data: () => ({
+        items: [
+            {question: "What did you do today?", answer:""},
+            {question: "Do you want to learn something new?", answer:""},
+            {question: "Anything to add?", answer:""},
+        ]
+    }),
 
-  components: {
-
-  }
 });
 </script>
